7f35416bf30cabe4872f961c2d0baa5eff38b9ee
jeff1901 <70955537+xhefribala@users.noreply.github.com>
2021-10-06 11:30:53 +1100

    Commit update testing

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working DIOCANE on your app!</Text>
+      <Text>Open up App.js to start working DIOCANE Hello on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
