b25d61a739439b470d92738df1ecafc9ffb01507
jeff1901 <70955537+xhefribala@users.noreply.github.com>
2021-10-13 10:15:19 +1100

    .

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-vector-icons/Icon';
 
 const App = () => {
   return (
     <SafeAreaView
-      style={{ flex: 1, backgroundColor: colours.white }}></SafeAreaView>);
+      style={{ flex: 1, backgroundColor: colours.white }}>
+      <View style={styles.header}>
+        <Text style={{ fontWeight: '700', fontSize: 25, color: colours.textColour }}>Xhefri's To Do List</Text>
+      </View>
+    </SafeAreaView>);
 };
 
 const colours = {
-  primary: '#2596be', white: '#fff'
+  textColour: '#2596be', white: '#fff', backColour: '#eeeee4'
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+
+
 export default App;
