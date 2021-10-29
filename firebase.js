9f86f7424522cd334cbe2769575fdc2078fc548d
jeff1901 <70955537+xhefribala@users.noreply.github.com>
2021-10-29 12:54:39 +1100

    Firebase Auth

--- firebase.js
+++ firebase.js
@@ -15,5 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 let app;
-if (firebase.apps.lenght)
-    const app = initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
