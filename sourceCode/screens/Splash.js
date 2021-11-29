a4e97cdbdc8f0d4fcc27bc57c191c63e13367a49
jeff1901 <70955537+xhefribala@users.noreply.github.com>
2021-11-29 16:39:04 +1100

    splash delay

--- sourceCode/screens/Splash.js
+++ sourceCode/screens/Splash.js
@@ -3,20 +3,20 @@ import { View, Image } from "react-native";
 import LogoImage from "../../assets/splash1.png";
 
 export default class Splash extends Component {
-
-    componentDidMount() {
-        //method that gets called after a component is mounted
-        setTimeout(() => {
-            this.props.navigation.navigate("Login");
-        }, 4000) //4 seconds 
-    }
-    render() {
-        return (
-            <View style={{ width: '100%', height: '100%' }}>
-                <Image style={{ width: '100%', height: '100%' }} source={LogoImage}></Image>
-            </View>
-        );
-    }
-
-
+  componentDidMount() {
+    //method that gets called after a component is mounted
+    setTimeout(() => {
+      this.props.navigation.navigate("Login");
+    }, 3000); //3 seconds
+  }
+  render() {
+    return (
+      <View style={{ width: "100%", height: "100%" }}>
+        <Image
+          style={{ width: "100%", height: "100%" }}
+          source={LogoImage}
+        ></Image>
+      </View>
+    );
+  }
 }
